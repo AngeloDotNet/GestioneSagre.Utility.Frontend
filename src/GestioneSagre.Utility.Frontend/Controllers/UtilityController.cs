dc1447419b422df0d57405a500f27c42c6554b28
Angelo Pirola <angelo9871@gmail.com>
2023-06-26 00:02:02 +0200

    Aggiornato progetto Controllers

--- src/GestioneSagre.Utility.Frontend/Controllers/UtilityController.cs
+++ src/GestioneSagre.Utility.Frontend/Controllers/UtilityController.cs
@@ -8,20 +8,24 @@ public class UtilityController : BaseController
     IRequestClient<TipoPagamentoListRequest> tipoPagamentoRequestClient;
     IRequestClient<TipoScontrinoListRequest> tipoScontrinoRequestClient;
 
+    private readonly ILoggerService logger;
+
     public UtilityController(IRequestClient<ScontrinoPagatoListRequest> scontrinoPagatoRequestClient,
-        IRequestClient<ScontrinoStatoListRequest> scontrinoStatoRequestClient,
-        IRequestClient<TipoClienteListRequest> tipoClienteRequestClient,
-        IRequestClient<TipoPagamentoListRequest> tipoPagamentoRequestClient,
-        IRequestClient<TipoScontrinoListRequest> tipoScontrinoRequestClient)
+        IRequestClient<ScontrinoStatoListRequest> scontrinoStatoRequestClient, IRequestClient<TipoClienteListRequest> tipoClienteRequestClient,
+        IRequestClient<TipoPagamentoListRequest> tipoPagamentoRequestClient, IRequestClient<TipoScontrinoListRequest> tipoScontrinoRequestClient,
+        ILoggerService logger)
     {
         this.scontrinoPagatoRequestClient = scontrinoPagatoRequestClient;
         this.scontrinoStatoRequestClient = scontrinoStatoRequestClient;
         this.tipoClienteRequestClient = tipoClienteRequestClient;
         this.tipoPagamentoRequestClient = tipoPagamentoRequestClient;
         this.tipoScontrinoRequestClient = tipoScontrinoRequestClient;
+        this.logger = logger;
     }
 
     [HttpGet("ScontrinoPagato")]
+    [ProducesResponseType(typeof(List<ScontrinoPagato>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetScontrinoPagatoAsync()
     {
         var result = new List<ScontrinoPagato>();
@@ -30,12 +34,25 @@ public class UtilityController : BaseController
         {
             var response = await request.GetResponse<ScontrinoPagatoListResponse>();
             result = response.Message.ScontriniPagati;
+
+            switch (result.Count)
+            {
+                case > 0:
+                    logger.SaveLogInformation($"Trovati {result.Count} record: ScontrinoPagato");
+                    break;
+                default:
+                    logger.SaveLogWarning("Nessun record trovato: ScontrinoPagato");
+                    break;
+            }
         }
 
-        return Ok(result);
+        return result.Count > 0 ? Ok(new DefaultResponse(true, result))
+            : throw new ExceptionResponse(HttpStatusCode.NotFound, 0, "NotFound", $"Nessun record trovato: ScontrinoPagato");
     }
 
     [HttpGet("ScontrinoStato")]
+    [ProducesResponseType(typeof(List<ScontrinoStato>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetScontrinoStatoAsync()
     {
         var result = new List<ScontrinoStato>();
@@ -44,12 +61,25 @@ public class UtilityController : BaseController
         {
             var response = await request.GetResponse<ScontrinoStatoListResponse>();
             result = response.Message.ScontriniStati;
+
+            switch (result.Count)
+            {
+                case > 0:
+                    logger.SaveLogInformation($"Trovati {result.Count} record: ScontrinoStato");
+                    break;
+                default:
+                    logger.SaveLogWarning("Nessun record trovato: ScontrinoStato");
+                    break;
+            }
         }
 
-        return Ok(result);
+        return result.Count > 0 ? Ok(new DefaultResponse(true, result))
+            : throw new ExceptionResponse(HttpStatusCode.NotFound, 0, "NotFound", $"Nessun record trovato: ScontrinoStato");
     }
 
     [HttpGet("TipoCliente")]
+    [ProducesResponseType(typeof(List<TipoCliente>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetTipoClienteAsync()
     {
         var result = new List<TipoCliente>();
@@ -58,12 +88,25 @@ public class UtilityController : BaseController
         {
             var response = await request.GetResponse<TipoClienteListResponse>();
             result = response.Message.TipoClienti;
+
+            switch (result.Count)
+            {
+                case > 0:
+                    logger.SaveLogInformation($"Trovati {result.Count} record: TipoCliente");
+                    break;
+                default:
+                    logger.SaveLogWarning("Nessun record trovato: TipoCliente");
+                    break;
+            }
         }
 
-        return Ok(result);
+        return result.Count > 0 ? Ok(new DefaultResponse(true, result))
+            : throw new ExceptionResponse(HttpStatusCode.NotFound, 0, "NotFound", $"Nessun record trovato: TipoCliente");
     }
 
     [HttpGet("TipoPagamento")]
+    [ProducesResponseType(typeof(List<TipoPagamento>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetTipoPagamentoAsync()
     {
         var result = new List<TipoPagamento>();
@@ -72,12 +115,25 @@ public class UtilityController : BaseController
         {
             var response = await request.GetResponse<TipoPagamentoListResponse>();
             result = response.Message.TipoPagamenti;
+
+            switch (result.Count)
+            {
+                case > 0:
+                    logger.SaveLogInformation($"Trovati {result.Count} record: TipoPagamento");
+                    break;
+                default:
+                    logger.SaveLogWarning("Nessun record trovato: TipoPagamento");
+                    break;
+            }
         }
 
-        return Ok(result);
+        return result.Count > 0 ? Ok(new DefaultResponse(true, result))
+            : throw new ExceptionResponse(HttpStatusCode.NotFound, 0, "NotFound", $"Nessun record trovato: TipoPagamento");
     }
 
     [HttpGet("TipoScontrino")]
+    [ProducesResponseType(typeof(List<TipoScontrino>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetTipoScontrinoAsync()
     {
         var result = new List<TipoScontrino>();
@@ -86,8 +142,19 @@ public class UtilityController : BaseController
         {
             var response = await request.GetResponse<TipoScontrinoListResponse>();
             result = response.Message.TipoScontrini;
+
+            switch (result.Count)
+            {
+                case > 0:
+                    logger.SaveLogInformation($"Trovati {result.Count} record: TipoScontrino");
+                    break;
+                default:
+                    logger.SaveLogWarning("Nessun record trovato: TipoScontrino");
+                    break;
+            }
         }
 
-        return Ok(result);
+        return result.Count > 0 ? Ok(new DefaultResponse(true, result))
+            : throw new ExceptionResponse(HttpStatusCode.NotFound, 0, "NotFound", $"Nessun record trovato: TipoScontrino");
     }
 }
--- src/GestioneSagre.Utility.Frontend/GlobalUsings.cs
+++ src/GestioneSagre.Utility.Frontend/GlobalUsings.cs
@@ -1,4 +1,5 @@
-﻿global using System.Net.Mime;
+﻿global using System.Net;
+global using System.Net.Mime;
 global using GestioneSagre.Utility.Frontend.BusinessLayer.MassTransit;
 global using GestioneSagre.Utility.Frontend.Controllers.Common;
 global using GestioneSagre.Utility.Shared.Entities;
@@ -7,4 +8,7 @@ global using GestioneSagre.Utility.Shared.Models.Response;
 global using MassTransit;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Server.Kestrel.Core;
+global using NET6CustomLibrary.CustomResponse;
+global using NET6CustomLibrary.Extensions;
+global using NET6CustomLibrary.Serilog.Services;
 global using NET6CustomLibrary.Swagger;
